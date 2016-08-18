748613beb34cddfb19d551ab2b9b1a6684a86c4e
naga-georgian <nagavenkatasarat.rimmalapudi@mygeorgian.ca>
2016-08-18 13:39:12 -0400

    Add project files.

---
+++ COMP2006-S2016-FinalExamV2/Controllers/ListPagesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using COMP2006_S2016_FinalExamV2.Models;
+using System.Data.Entity;
+using System.Threading.Tasks;
+//Anthony Gobin
+// ListPagesController
+//Final Exam
+namespace COMP2006_S2016_FinalExamV2.Controllers
+{
+    public class ListPagesController : Controller
+    {
+        private MVCMusicStoreContext db = new MVCMusicStoreContext();
+        // GET: ListPages
+        public async Task<ActionResult> Album()
+        {
+            return View(await db.Albums.ToListAsync());
+        }
+        public async Task<ActionResult> Artist()
+        {
+            return View(await db.Artists.ToListAsync());
+        }
+        public async Task<ActionResult> Genre()
+        {
+            return View(await db.Genres.ToListAsync());
+        }
+    }
+}
